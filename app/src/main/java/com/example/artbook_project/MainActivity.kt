3dd629916c7de4de9264c1df86814fb1413eb2c5
edizzk <edizmetekurt@gmail.com>
2022-06-21 19:56:24 +0300

    getting items from database and listing

--- app/src/main/java/com/example/artbook_project/MainActivity.kt
+++ app/src/main/java/com/example/artbook_project/MainActivity.kt
@@ -1,21 +1,53 @@
 package com.example.artbook_project
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.artbook_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var artList : ArrayList<Art>
+    private lateinit var artAdapter : ArtAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        artList = ArrayList<Art>()
+        artAdapter = ArtAdapter(artList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = artAdapter
+
+        try {
+
+            val database = this.openOrCreateDatabase("Arts", Context.MODE_PRIVATE,null)
+
+            val cursor = database.rawQuery("SELECT * FROM arts",null)
+            val artNameIx = cursor.getColumnIndex("artname")
+            val idIx = cursor.getColumnIndex("id")
+
+            while (cursor.moveToNext()) {
+                val name = cursor.getString(artNameIx)
+                val id = cursor.getInt(idIx)
+                val art = Art(name,id)
+                artList.add(art)
+            }
+
+            cursor.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
