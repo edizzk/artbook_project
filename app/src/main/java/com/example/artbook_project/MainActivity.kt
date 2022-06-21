050ba433084a502e3229c9fceca758de9b6cdded
edizzk <edizmetekurt@gmail.com>
2022-06-21 14:38:11 +0300

    create menu options method

--- app/src/main/java/com/example/artbook_project/MainActivity.kt
+++ app/src/main/java/com/example/artbook_project/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.artbook_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.artbook_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_art_item) {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onContextItemSelected(item)
     }
 }
